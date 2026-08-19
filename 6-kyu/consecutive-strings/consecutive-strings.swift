6ddec042938b06a147d8b1e62d021e1900e6794f
StephanieBallard <58450565+StephanieBallard@users.noreply.github.com>
2026-08-18 22:34:25 -0700

    Update solution - CodeHub

--- 6-kyu/consecutive-strings/consecutive-strings.swift
+++ 6-kyu/consecutive-strings/consecutive-strings.swift
@@ -1,19 +1,25 @@
+// This version evades counting the same words twice when the window slides across the array
 func longestConsec(_ strarr: [String], _ k: Int) -> String {
     guard !strarr.isEmpty, k <= strarr.count, k > 0 else { return "" }
 ​
-    var largestStringCount = 0
-    var longestWord = ""
+    var currentLength = strarr[0..<k].reduce(0) { $0 + $1.count }
+    var largestStringCount = currentLength
+    var firstWindow = 0
 ​
-    for start in 0...(strarr.count - k) {
-        let end = start + k
-        let window = strarr[start..<end]
-        let word = window.joined()
+    for start in 1..<(strarr.count - k + 1) {
+        let oldIndex = start - 1
+        let newIndex = start + k - 1
 ​
-        if word.count > largestStringCount {
-            longestWord = word
-            largestStringCount = word.count
+        currentLength -= strarr[oldIndex].count
+        currentLength += strarr[newIndex].count
+​
+        if currentLength > largestStringCount {
+            largestStringCount = currentLength
+            firstWindow = start
         }
     }
 ​
-    return longestWord
+    let end = firstWindow + k
+    return strarr[firstWindow..<end].joined()
 }
+​
