c75fa972980030c2582c86a973d4dbb05434cd6a
StephanieBallard <58450565+StephanieBallard@users.noreply.github.com>
2026-08-19 17:41:58 -0700

    Update solution - CodeHub

--- 8-kyu/opposite-number/opposite-number.swift
+++ 8-kyu/opposite-number/opposite-number.swift
@@ -1,3 +1,5 @@
+// removing extra code that's accounted for from double negative logic
 func opposite(number: Double) -> Double {
-    return number < 0 ? abs(number) : -number
+//     return number < 0 ? abs(number) : -number
+    return -number
  }
