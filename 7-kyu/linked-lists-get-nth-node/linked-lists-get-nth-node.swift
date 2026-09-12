606dc9e587c2dfe1c1b93ec64d1b2af2e8212fda
StephanieBallard <58450565+StephanieBallard@users.noreply.github.com>
2026-09-11 23:01:40 -0700

    Add solution - CodeHub

---
+++ 7-kyu/linked-lists-get-nth-node/linked-lists-get-nth-node.swift
@@ -0,0 +1,28 @@
+class Node {
+    var data: Int
+    var next: Node?
+    init(_ data: Int) {
+        self.data = data
+    }
+}
+​
+enum LinkedListError: Error {
+    case invalidIndex
+}
+​
+func getNth(_ head: Node?, _ index: Int) throws -> Node? {
+    var current = head
+    var counter = 0
+​
+    while let node = current {
+        if counter == index {
+            return node
+        }
+​
+        counter += 1
+        current = node.next
+    }
+​
+    throw LinkedListError.invalidIndex
+}
+​
